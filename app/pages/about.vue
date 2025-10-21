5c62a2216ef5a370cae3295591dacd85cc19e0fd
Jimena <j.miguez@airlst.com>
2025-10-21 10:33:38 +0200

    add images

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,14 +1,19 @@
 <template>
   <section class="space-y-4 container">
-    <h1 class="text-3xl font-heading font-semibold text-slate-900">
-      {{ t('about.title') }}
-    </h1>
-    <p class="text-base text-slate-500">
-      {{ t('about.intro') }}
-    </p>
-    <p class="text-base text-slate-500">
-      {{ t('about.details') }}
-    </p>
+    <div class="grid gri lg:grid-cols-2 gap-6">
+      <img src="/images/sample.jpg" alt="">
+      <article>
+        <h1 class="text-3xl font-heading font-semibold text-slate-900">
+          {{ t('about.title') }}
+        </h1>
+        <p class="text-base text-slate-500">
+          {{ t('about.intro') }}
+        </p>
+        <p class="text-base text-slate-500">
+          {{ t('about.details') }}
+        </p>
+      </article>
+    </div>
   </section>
 </template>
 
