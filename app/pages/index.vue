ea409f90a4abfc7540f713ed0743b9725830459d
Jimena <j.miguez@airlst.com>
2025-10-26 14:55:59 +0100

    sample t-shirts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,14 +7,19 @@
       </h1>
       <div class="hero-remera mt-10">
         <img
-          src="/images/remera-lista-one-transparent.png"
-          alt="Remera diseño uno"
+          src="/images/remera-lista-azul.png"
+          alt="Remera color azul"
           class="hero-remera__image hero-remera__image--primary"
         />
         <img
-          src="/images/remera-lista-2-transparente.png"
-          alt="Remera diseño dos"
+          src="/images/remera-lista-negra.png"
+          alt="Remera color negra"
           class="hero-remera__image hero-remera__image--secondary"
+        />
+        <img
+          src="/images/remera-lista-blanca.png"
+          alt="Remera color blanca"
+          class="hero-remera__image hero-remera__image--tertiary"
         />
       </div>
     </div>
@@ -24,6 +29,5 @@
 <script setup>
 
 const { t } = useI18n()
-
 
 </script>
